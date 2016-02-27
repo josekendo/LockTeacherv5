c192a4f4c00aff8351ca12812b921c2181391aae
josekendo <josevicenteantoncoy@hotmail.com>
2016-02-27 06:36:18 +0100

    se pone el inicio

--- app/src/main/java/com/al/josev/lockteacher/Configuracion.java
+++ app/src/main/java/com/al/josev/lockteacher/Configuracion.java
@@ -2,12 +2,20 @@ package com.al.josev.lockteacher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Configuracion extends AppCompatActivity {
+    Spinner day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracion);
+        day = (Spinner)findViewById(R.id.spinne);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.asig, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        day.setAdapter(adapter);
         setContentView(R.layout.activity_configuracion);
     }
 }
