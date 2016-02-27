2fea4e77d79d02fdbb54d98dd5da9e9b2ca24873
josekendo <josevicenteantoncoy@hotmail.com>
2016-02-27 01:31:22 +0100

    Limpieza de R

--- app/src/main/java/com/al/josev/lockteacher/Login_Activity.java
+++ app/src/main/java/com/al/josev/lockteacher/Login_Activity.java
@@ -2,12 +2,38 @@ package com.al.josev.lockteacher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login_Activity extends AppCompatActivity {
-
+    EditText nombre;
+    EditText pin1;
+    EditText pin2;
+    Button boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
+
+
+        //se declara las variables del reegistro
+        nombre=(EditText)findViewById(R.id.input_nom);
+        pin1=(EditText)findViewById(R.id.input_pass);
+        pin2=(EditText)findViewById(R.id.input_pass3);
+
+        //se declara el boton
+        boton=(Button)findViewById(R.id.seguir);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                //comprobamos los datos del nombre
+                if(nombre.getText().length() > 0)
+                {
+
+                }
+            }
+        });
     }
 }
