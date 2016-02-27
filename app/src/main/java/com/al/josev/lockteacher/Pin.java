484dd51cad927284c0728b674cff825bcce3c6b4
josekendo <josevicenteantoncoy@hotmail.com>
2016-02-27 07:24:58 +0100

    implemento.... a medias pin

--- app/src/main/java/com/al/josev/lockteacher/Pin.java
+++ app/src/main/java/com/al/josev/lockteacher/Pin.java
@@ -3,6 +3,8 @@ package com.al.josev.lockteacher;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.al.josev.lockteacher.Clases.Lector;
@@ -11,17 +13,18 @@ import java.io.File;
 import java.io.IOException;
 
 public class Pin extends AppCompatActivity {
-
+    String [] info;
+    EditText nombre,pin1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pin);
-        EditText nombre=(EditText)findViewById(R.id.input_nom2);
-        EditText pin1=(EditText)findViewById(R.id.pin_input);
+        nombre=(EditText)findViewById(R.id.input_nom2);
+        pin1=(EditText)findViewById(R.id.pin_input);
         String filename="user.txt";
         File file = new File(getFilesDir(), filename);
         Lector log = new Lector();
-        String [] info= new String[2];
+        info= new String[2];
         info[0]="";
         info[1]="";
         try {
@@ -30,10 +33,21 @@ public class Pin extends AppCompatActivity {
         {
 
         }
-        if(info[0].compareToIgnoreCase(nombre.getText().toString()) == 0 && info[1].compareToIgnoreCase(pin1.getText().toString()) == 0)
+        Button boton2=(Button)findViewById(R.id.button2);
+        boton2.setOnClickListener(new View.OnClickListener()
         {
-            Intent t = new Intent(this,Configuracion.class);
-            startActivity(t);
-        }
+            @Override
+            public void onClick(View v)
+            {
+
+                if(info[0].compareToIgnoreCase(nombre.getText().toString()) == 0 && info[1].compareToIgnoreCase(pin1.getText().toString()) == 0)
+                {
+                    Intent t = new Intent(getApplicationContext(),Configuracion.class);
+                    startActivity(t);
+                }
+
+            }
+        });
     }
+
 }
