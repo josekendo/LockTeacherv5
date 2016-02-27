bc047e9ac13d8370213d78e463937180d3d81b7c
Shizer <dlh2@alu.ua.es>
2016-02-27 03:20:36 +0100

    Añadir las 3 Activitys

---
+++ app/src/main/java/com/al/josev/lockteacher/Pin.java
@@ -0,0 +1,13 @@
+package com.al.josev.lockteacher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin);
+    }
+}
