12ac1bbb3b744d95ac936dc8300bfe716c10b453
josekendo <josevicenteantoncoy@hotmail.com>
2016-02-27 06:54:33 +0100

    diseño terminado ¿Shizer el logo arriba?

--- app/src/main/java/com/al/josev/lockteacher/Pin.java
+++ app/src/main/java/com/al/josev/lockteacher/Pin.java
@@ -2,6 +2,7 @@ package com.al.josev.lockteacher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Pin extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Pin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pin);
+        EditText nombre=(EditText)findViewById(R.id.input_nom2);
+        EditText pin1=(EditText)findViewById(R.id.pin_input);
+
     }
 }
